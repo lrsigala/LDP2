6692668942c054a680e63ef10a0d84fb87b2f660
Luis Raúl Sigala <143978553+lrsigala@users.noreply.github.com>
2024-04-09 15:12:04 -0600

    Cambios 3

--- src/app/App/regpa/page.js
+++ src/app/App/regpa/page.js
@@ -1,30 +1,65 @@
 'use client'
 import { useEffect, useState } from "react"
-
+import ButtonAction from "@/components/atoms/ButtonAction"
+import api from "../../../../lib/firebase/api"
 export default function RegPa() {
     const [datos, setDatos] = useState({
-        nombre:'',
-        edad:'',
-        peso:'',
-        triage:'',
+        nombre: '',
+        edad: '',
+        peso: '',
+        triage: '',
     })
-
+    const estilo1 = "w-full p-1 border border-cyan-500 mt-2"
+    const valoresTriage = [
+        {
+            key: '1',
+            nombre: 'Resucitacion',
+            color: 'bg-red-500'
+        }, {
+            key: '2',
+            nombre: 'Emergencia',
+            color: 'bg-orange-500'
+        }, {
+            key: '3',
+            nombre: 'Urgencia',
+            color: 'bg-yellow-200'
+        }, {
+            key: '4',
+            nombre: 'Menos urgente',
+            color: 'bg-green-500'
+        }, {
+            key: '5',
+            nombre: 'No urgente',
+            color: 'bg-blue-500'
+        }
+    ]
+    const buttonTriage = ({ key, nombre, color }) => {
+        return (
+            <button onClick={() => { setDatos({ ...datos, ["triage"]: key }) }} className={`${color} ${key === datos.triage && 'border-2 border-black'}`}>{nombre}</button>
+        )
+    }
+    const save = ()=>{
+        api.savePatient(datos)
+    }
     return (
         <div className="flex h-screen w-screen  items-center justify-center bg-black/10 p-3">
             <div className="flex flex-col h-full w-full items-center justify-center bg-white/90 rounded">
-                <form className="flex flex-col w-1/2 p-10">
-                    <h1 className="text-xl text-center border-b border-cyan-500 mb-2">Ingresa tus datos</h1>
+                <div className="flex flex-col w-1/2 p-10">
+                    <h1 className="text-xl text-center border-b border-cyan-500 mb-2">Ingresa los datos del paciente:</h1>
                     <label>Nombre:</label>
-                    <input value={credenciales.nombre} onChange={(value) => { setCredenciales({ ...credenciales, ["nombre"]: value.target.value }) }} className={estilo1} />
-                    <label>Telefono:</label>
-                    <input value={credenciales.telefono} onChange={(value) => { setCredenciales({ ...credenciales, ["telefono"]: value.target.value }) }} className={estilo1} />
-                    <label>Cuenta o Correo Electrónico:</label>
-                    <input value={credenciales.email} onChange={(value) => { setCredenciales({ ...credenciales, ["email"]: value.target.value }) }} className={estilo1} />
-                    <label>Contraseña:</label>
-                    <input value={credenciales.password} onChange={(value) => { setCredenciales({ ...credenciales, ["password"]: value.target.value }) }} type="password" className={`${estilo1}`} />
-                    <label>Confirmar Contraseña:</label>
-                    <input value={credenciales.confirmar} onChange={(value) => { setCredenciales({ ...credenciales, ["confirmar"]: value.target.value }) }} type="password" className={`${estilo1}`} />
-                </form>
+                    <input value={datos.nombre} onChange={(value) => { setDatos({ ...datos, ["nombre"]: value.target.value }) }} className={estilo1} />
+                    <label>Edad:</label>
+                    <input value={datos.edad} onChange={(value) => { setDatos({ ...datos, ["edad"]: value.target.value }) }} className={estilo1} />
+                    <label>Peso:</label>
+                    <input value={datos.peso} onChange={(value) => { setDatos({ ...datos, ["peso"]: value.target.value }) }} className={estilo1} />
+                    <label>Triage:</label>
+                    <div className="flex w-full justify-between">
+                        {valoresTriage.map((item, index) => buttonTriage(item))}
+                    </div>
+                    <div className="flex w-full justify-evenly mt-10">
+                        <ButtonAction title={"Guardar Paciente"} func={save} />
+                    </div>
+                </div>
             </div>
         </div>
     )
